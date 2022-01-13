9a9422ddd8359a0d590fb033f5840409c4b9bb3b
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-13 23:34:18 +0530

    Added eventlist & item component

---
+++ components/events/event-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EventItem from './event-item';
+
+export default function EventList(props) {
+    const { items } = props;
+    return (
+        <ul>
+            {items.map(event => <EventItem />)}
+        </ul>
+    )
+}

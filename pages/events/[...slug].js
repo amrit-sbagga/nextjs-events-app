02d975453056cb19209931fa92028fdabec26fd8
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-15 23:12:03 +0530

    Extracting filter data on slug page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
 
 export default function FilteredEventsPage() {
-    return (
-        <div>
-            <h1>Filtered Events Page</h1>
-        </div>
-    )
+  const router = useRouter();
+  const filterData = router.query.slug;
+  //console.log(filterData);
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = parseInt(filterData[0]);
+  const filteredMonth = parseInt(filterData[1]);
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth > 12 ||
+    filteredMonth < 1
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+  
+  if(!filteredEvents || filteredEvents.length === 0){
+    return <p>No events found for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+      <h1>Filtered Events Page</h1>
+    </div>
+  );
 }

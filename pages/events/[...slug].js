197043403ffbfc6c2fc1b039f86894cab4dc0ca9
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-17 17:08:37 +0530

    Improved swr fetcher

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -133,13 +133,29 @@ export default function FilteredEventsPage(props) {
   );
 }
 
-function fetcher(url) {
-  return fetch(url, {
-    headers: {
-      // Authorization: `Bearer ${localStorage.getItem('token')}`,
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json());
+// function fetcher(url) {
+//   return fetch(url, {
+//     headers: {
+//       // Authorization: `Bearer ${localStorage.getItem('token')}`,
+//       "Content-Type": "application/json",
+//     },
+//   }).then((response) => response.json());
+// }
+
+const fetcher = async url => {
+  const res = await fetch(url)
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.info = await res.json()
+    error.status = res.status
+    throw error
+  }
+
+  return res.json()
 }
 
 // these is replaced with client side data fetching
